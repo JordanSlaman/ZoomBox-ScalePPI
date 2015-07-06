ab14cc0c44984c417b512945cc6e66837f7971d3
Jordan <jordan.slaman@gmail.com>
2015-07-06 14:26:20 -0600

    Initial Commit
    Simple Zoombox, two galleries with 2 images.

---
+++ js/demoscript.js
@@ -0,0 +1,17 @@
+jQuery(function($){
+    $('a.zoombox').zoombox();
+
+    /**
+    * Or You can also use specific options
+    $('a.zoombox').zoombox({
+        theme       : 'zoombox',        //available themes : zoombox,lightbox, prettyphoto, darkprettyphoto, simple
+        opacity     : 0.8,              // Black overlay opacity
+        duration    : 800,              // Animation duration
+        animation   : true,             // Do we have to animate the box ?
+        width       : 600,              // Default width
+        height      : 400,              // Default height
+        gallery     : true,             // Allow gallery thumb view
+        autoplay : false                // Autoplay for video
+    });
+    */
+});
